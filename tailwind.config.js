2890f01d0a7d82372e5f62183423a018067f2adc
williamIIImarpa <act.wimarpa@gmail.com>
2024-07-29 21:50:51 +0800

    first commit for home ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./dist/index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
+  },
+  variants: {
+    extend: {
+      display: ['group-focus']
+    }
   },
   plugins: [],
 }
